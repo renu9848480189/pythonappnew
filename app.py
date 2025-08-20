a002b59a64b115eeaf856d57fd3f4067f8e6b6ac
renu_oss <129938230+renu9848480189@users.noreply.github.com>
2025-08-20 14:07:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def counter():
-    return 'Hello Python!!!!'
+    return 'Hello Python!!!'
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000)
